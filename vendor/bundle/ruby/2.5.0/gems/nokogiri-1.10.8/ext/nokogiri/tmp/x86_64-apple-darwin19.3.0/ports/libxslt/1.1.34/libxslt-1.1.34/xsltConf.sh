d878abba085d544bbccddd44066e892167d2730d
lefokd <llllivagin0528@gmail.com>
2020-02-28 11:08:21 +0000

    testdeploy

--- vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.8/ext/nokogiri/tmp/x86_64-apple-darwin19.3.0/ports/libxslt/1.1.34/libxslt-1.1.34/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/okadalef/Desktop/SweetsProto/vagrant/TAGNERD/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.8/ports/x86_64-apple-darwin19.3.0/libxslt/1.1.34/lib"
-XSLT_LIBS="-lxslt -L/Users/okadalef/Desktop/SweetsProto/vagrant/TAGNERD/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.8/ports/x86_64-apple-darwin19.3.0/libxml2/2.9.10/lib -lxml2 -lz -lpthread -liconv -lm "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/Users/okadalef/Desktop/SweetsProto/vagrant/TAGNERD/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.8/ports/x86_64-apple-darwin19.3.0/libxslt/1.1.34/include"
-MODULE_VERSION="xslt-1.1.34"
